1731a04612336c04a1220736b39e0551331d44fb
Swaranjali Patil <shubhangippatil2020@gmail.com>
2026-08-22 09:47:50 +0530

    Add perimeter calculation for a square

---
+++ perimeter.cpp
@@ -0,0 +1,11 @@
+  #include <iostream>
+using namespace std;
+
+int main() {
+    int side = 6;
+    
+    int perimeter = 4 * side;
+    
+    cout << "perimeter of square is: " << perimeter;
+    return 0;
+}
