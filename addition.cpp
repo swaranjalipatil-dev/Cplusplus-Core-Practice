bb175d3b2d73cdeb662075ed5f703e58ddc6979a
Swaranjali Patil <shubhangippatil2020@gmail.com>
2026-08-23 11:02:43 +0530

    Implement basic addition program in C++

---
+++ addition.cpp
@@ -0,0 +1,13 @@
+  #include <iostream>
+using namespace std;
+
+int main() {
+    int a, b;
+    cout << "Enter to numbers: ";
+    cin >> a >> b;
+
+    int sum = a + b;
+    cout << "Total sum is: " << sum;
+
+ return 0;
+}
