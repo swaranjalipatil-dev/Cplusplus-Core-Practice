3c254f79bb7f3c1be4467547efa518d97934637f
Swaranjali Patil <shubhangippatil2020@gmail.com>
2026-09-18 10:33:43 +0530

    Add basic C++ program to print a message
    Added a simple C++ program that prints a motivational message.

---
+++ smallcode c++.cpp
@@ -0,0 +1,35 @@
+  Stop copy pasting code you don't actually understand
+Build the coding confidence you need to become a developer companies will fight for
+Become a PRO
+×
+Programiz
+C++ Online Compiler
+
+Programiz PRO ❯
+main.cpp
+
+Run
+1
+2
+3
+4
+5
+6
+7
+8
+9
+// Online C++ compiler to run C++ program online
+#include <iostream>
+
+int main() {
+    // Write C++ code here
+    std::cout << "Start small. Ship something.";
+
+    return 0;
+}
+Output
+
+
+Start small. Ship something.
+
+=== Code Execution Successful ===
